e454e58d9bc5f659745c8349b2d332ce31f92034
haruki7049 <tontonkirikiri@gmail.com>
2026-08-05 13:03:55 +0900

    Update sine module

--- build.zig
+++ build.zig
@@ -17,14 +17,15 @@ pub fn build(b: *std.Build) !void {
         },
     });
 
+    const imports: []const std.Build.Module.Import = &.{
+        .{ .name = "lightmix", .module = lightmix.module("lightmix") },
+        .{ .name = "sine", .module = sine },
+    };
     const mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
-        .imports = &.{
-            .{ .name = "lightmix", .module = lightmix.module("lightmix") },
-            .{ .name = "sine", .module = sine },
-        },
+        .imports = imports,
     });
 
     // Library installation
@@ -63,4 +64,43 @@ pub fn build(b: *std.Build) !void {
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_sine_tests.step);
     test_step.dependOn(&run_mod_tests.step);
+
+    // Sandbox
+    const sandbox_step = b.step("sandbox", "Generate wav files on sandbox");
+    try build_sandbox(b, target, optimize, imports, sandbox_step);
+}
+
+fn build_sandbox(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    imports: []const std.Build.Module.Import,
+    sandbox_step: *std.Build.Step,
+) !void {
+    const paths_names: []const struct { []const u8, []const u8 } = &.{
+        .{ "sandbox/sine/mono-440.0.zig", "sine-mono-440.0.wav" },
+        .{ "sandbox/sine/stereo-440.0.zig", "sine-stereo-440.0.wav" },
+    };
+
+    inline for (paths_names) |pn| {
+        const path = pn.@"0";
+        const name = pn.@"1";
+
+        const mod = b.createModule(.{
+            .root_source_file = b.path(path),
+            .target = target,
+            .optimize = optimize,
+            .imports = imports,
+        });
+
+        const wave = try l.addWave(b, mod, .{
+            .format = .{ .wav = .{
+                .bits = 16,
+                .format_code = .pcm,
+                .name = name,
+            } },
+            .path = .{ .custom = "share/sandbox" },
+        });
+        sandbox_step.dependOn(wave.step);
+    }
 }
---
+++ sandbox/sine/mono-440.0.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const lightmix = @import("lightmix");
+const sine = @import("sine");
+const T = f64;
+
+pub fn gen(allocator: std.mem.Allocator) !lightmix.Wave(T) {
+    const FREQUENCY: T = 440.0;
+    const SAMPLE_RATE: u32 = 44100;
+    const CHANNELS: u16 = 1;
+    const LENGTH: usize = SAMPLE_RATE * 2;
+    const VOLUME: T = 1.0;
+
+    const sine_440: lightmix.Wave(T) = try sine.gen(T, allocator, FREQUENCY, SAMPLE_RATE, CHANNELS, LENGTH, VOLUME);
+    return sine_440;
+}
