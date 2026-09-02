f03d8cd21173a5561150156e0d095d2be0d3da6d
haruki7049 <tontonkirikiri@gmail.com>
2026-09-02 21:49:08 +0900

    Update

---
+++ modules/phrases/0002/root.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const lightmix = @import("lightmix");
+const filters = @import("filters");
+const utils = @import("utils");
+const synthesizers = @import("synthesizers");
+
+const Scale = utils.scale.Scale;
+const KarplusStrong = synthesizers.karplus_strong.KarplusStrong;
+const spb = utils.tempo.spb;
+
+pub fn gen(
+    comptime T: type,
+    allocator: std.mem.Allocator,
+    bpm: usize,
+    sample_rate: u32,
+    channels: u16,
+    volume: T,
+) !lightmix.Wave(T) {
+    const frequency: T = Scale.gen(.{ .code = .c, .octave = 3 });
+    const length: usize = spb(bpm, sample_rate);
+
+    const sound: lightmix.Wave(T) = try KarplusStrong.gen(T, allocator, frequency, sample_rate, channels, length, volume, .{
+        .feedback = 0.995,
+        .filter_weight = 0.8, // Adjust high-frequency damping
+        .excitation_lpf_passes = 24, // Apply low-pass filter passes to initial excitation noise for deep bass pluck
+    });
+    return sound;
+}
+
+test {
+    std.testing.refAllDecls(@This());
+}
--- modules/phrases/root.zig
+++ modules/phrases/root.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub const _0000 = @import("./0000/root.zig");
 pub const _0001 = @import("./0001/root.zig");
+pub const _0002 = @import("./0002/root.zig");
 
 test {
     std.testing.refAllDecls(@This());
