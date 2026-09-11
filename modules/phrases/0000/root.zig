b9aca8cc8780d37ffd4fedfa39a99e59dabd586f
haruki7049 <tontonkirikiri@gmail.com>
2026-09-11 20:31:46 +0900

    feat(phrases): implement 4-bar guitar arpeggio in 0000 intro

--- modules/phrases/0000/root.zig
+++ modules/phrases/0000/root.zig
@@ -5,9 +5,56 @@ const utils = @import("utils");
 const synthesizers = @import("synthesizers");
 
 const Scale = utils.scale.Scale;
-const Sine = synthesizers.sine.Sine;
-const Splitter = utils.splitter.Splitter;
+const KarplusStrong = synthesizers.karplus_strong.KarplusStrong;
 const spb = utils.tempo.spb;
+
+const Note = struct {
+    code: utils.scale.Code,
+    octave: usize,
+};
+
+/// 4-bar acoustic guitar arpeggio progression: | Fmaj7 | Em7 | Dm7 | Cmaj7 |
+const PROGRESSION = [_]Note{
+    // Bar 1: Fmaj7 (F3, C4, A3, E4, C4, A3, E4, C4)
+    .{ .code = .f, .octave = 3 },
+    .{ .code = .c, .octave = 4 },
+    .{ .code = .a, .octave = 3 },
+    .{ .code = .e, .octave = 4 },
+    .{ .code = .c, .octave = 4 },
+    .{ .code = .a, .octave = 3 },
+    .{ .code = .e, .octave = 4 },
+    .{ .code = .c, .octave = 4 },
+
+    // Bar 2: Em7 (E3, B3, G3, D4, B3, G3, D4, B3)
+    .{ .code = .e, .octave = 3 },
+    .{ .code = .b, .octave = 3 },
+    .{ .code = .g, .octave = 3 },
+    .{ .code = .d, .octave = 4 },
+    .{ .code = .b, .octave = 3 },
+    .{ .code = .g, .octave = 3 },
+    .{ .code = .d, .octave = 4 },
+    .{ .code = .b, .octave = 3 },
+
+    // Bar 3: Dm7 (D3, A3, F3, C4, A3, F3, C4, A3)
+    .{ .code = .d, .octave = 3 },
+    .{ .code = .a, .octave = 3 },
+    .{ .code = .f, .octave = 3 },
+    .{ .code = .c, .octave = 4 },
+    .{ .code = .a, .octave = 3 },
+    .{ .code = .f, .octave = 3 },
+    .{ .code = .c, .octave = 4 },
+    .{ .code = .a, .octave = 3 },
+
+    // Bar 4: Cmaj7 (C3, G3, E3, B3, G3, E3, B3, G3)
+    .{ .code = .c, .octave = 3 },
+    .{ .code = .g, .octave = 3 },
+    .{ .code = .e, .octave = 3 },
+    .{ .code = .b, .octave = 3 },
+    .{ .code = .g, .octave = 3 },
+    .{ .code = .e, .octave = 3 },
+    .{ .code = .b, .octave = 3 },
+    .{ .code = .g, .octave = 3 },
+};
 
 pub fn gen(
     comptime T: type,
@@ -17,62 +64,37 @@ pub fn gen(
     channels: u16,
     volume: T,
 ) !lightmix.Wave(T) {
-    const freq: T = Scale.gen(.{ .code = .c, .octave = 4 });
+    var composer = lightmix.Composer(T).init(allocator, .{
+        .channels = channels,
+        .sample_rate = sample_rate,
+    });
+    defer composer.deinit();
 
-    const long_spb: usize = @intFromFloat(@as(
-        f64,
-        @as(f64, @floatFromInt(spb(bpm, sample_rate))) * 4,
-    ));
-    var long = try Sine.gen(T, allocator, freq, sample_rate, channels, long_spb, volume);
-    defer long.deinit();
-    try filters.decay(T, &long);
+    const beat_frames = spb(bpm, sample_rate);
+    const step_frames = beat_frames / 2; // 8th note interval
+    const note_duration = beat_frames; // 1 beat natural decay per pluck
 
-    var short = try Sine.gen(T, allocator, freq, sample_rate, channels, spb(bpm, sample_rate) / 2, volume);
-    defer short.deinit();
-    try filters.decay(T, &short);
+    for (PROGRESSION, 0..) |note, i| {
+        const freq: T = Scale.gen(.{ .code = note.code, .octave = note.octave });
+        // Emphasize root note on the downbeat of each bar
+        const is_root = (i % 8 == 0);
+        const note_vol: T = if (is_root) volume * 1.0 else volume * 0.75;
 
-    return try Splitter.gen(
-        T,
-        allocator,
-        spb(bpm, sample_rate) * 16,
-        &.{
-            long,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            short,
-            short,
-            short,
-            short,
+        var pluck = try KarplusStrong.gen(T, allocator, freq, sample_rate, channels, note_duration, note_vol, .{
+            .feedback = 0.993,
+            .filter_weight = 0.5,
+            .excitation_lpf_passes = if (is_root) 2 else 0,
+        });
+        defer pluck.deinit();
 
-            long,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            short,
-            short,
-            short,
-            short,
-        },
-        sample_rate,
-        channels,
-    );
+        const start_sample = i * step_frames * channels;
+        try composer.append(.{
+            .wave = pluck,
+            .start_point = start_sample,
+        });
+    }
+
+    return try composer.finalize(.{});
 }
 
 test {
