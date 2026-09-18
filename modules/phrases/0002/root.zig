8bd10c44e1a14b625692b975b884d13f5bb706f4
haruki7049 <tontonkirikiri@gmail.com>
2026-09-18 23:55:26 +0900

    refactor(phrase): stream phrase events directly to Sequencer instead of pre-rendering Wave

--- modules/phrases/0002/root.zig
+++ modules/phrases/0002/root.zig
@@ -2,7 +2,48 @@ const std = @import("std");
 const lightmix = @import("lightmix");
 const utils = @import("utils");
 
-const phrase_data: utils.phrase.Phrase(f64, utils.scale.Scale) = @import("./phrase.zon");
+pub const phrase_data: utils.phrase.Phrase(f64, utils.scale.Scale) = @import("./phrase.zon");
+
+pub fn toEvents(
+    comptime T: type,
+    comptime ScaleGen: type,
+    allocator: std.mem.Allocator,
+    bpm: usize,
+    sample_rate: u32,
+) ![]utils.note.Note(T) {
+    return try phrase_data.toEvents(ScaleGen, allocator, bpm, sample_rate);
+}
+
+pub fn load(
+    comptime T: type,
+    comptime SoundGen: type,
+    comptime ScaleGen: type,
+    seq: *utils.sequencer.Sequencer(T),
+    target_track: *utils.sequencer.Track(T),
+    start_position: utils.sequencer.Position,
+    volume: T,
+) !void {
+    const events = try toEvents(T, ScaleGen, seq.allocator, seq.bpm, seq.sample_rate);
+    defer seq.allocator.free(events);
+
+    for (events) |event| {
+        var pos = event.position;
+        pos.bar += start_position.bar;
+        pos.beat += start_position.beat;
+
+        const note_wave = try SoundGen.gen(
+            T,
+            seq.allocator,
+            event.freq,
+            seq.sample_rate,
+            seq.channels,
+            event.length,
+            volume * event.volume,
+            .{},
+        );
+        try seq.addWave(target_track, note_wave, pos);
+    }
+}
 
 pub fn gen(
     comptime T: type,
@@ -14,14 +55,11 @@ pub fn gen(
     channels: u16,
     volume: T,
 ) !lightmix.Wave(T) {
-    const events = try phrase_data.toEvents(ScaleGen, allocator, bpm, sample_rate);
-    defer allocator.free(events);
-
     var seq = utils.sequencer.Sequencer(T).init(allocator, bpm, .{}, sample_rate, channels);
     defer seq.deinit();
 
     const track = try seq.createTrack(phrase_data.name);
-    try seq.addEvents(track, SoundGen, events, volume);
+    try load(T, SoundGen, ScaleGen, &seq, track, .{}, volume);
 
     return try seq.render();
 }
