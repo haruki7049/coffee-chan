9f4687b7e94464faac5f58b81b335a8fac5098db
haruki7049 <tontonkirikiri@gmail.com>
2026-09-19 14:52:29 +0900

    feat(synthesizers): add FM pizzicato wood bass synthesizer

--- modules/synthesizers/root.zig
+++ modules/synthesizers/root.zig
@@ -4,6 +4,7 @@ pub const sine = @import("./sine/root.zig");
 pub const whitenoise = @import("./whitenoise/root.zig");
 pub const karplus_strong = @import("./karplus-strong/root.zig");
 pub const rhodes = @import("./rhodes/root.zig");
+pub const wood_bass = @import("./wood_bass/root.zig");
 
 test {
     std.testing.refAllDecls(@This());
