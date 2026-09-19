3765b6bb972343d40f917134411df1f19c1b0703
haruki7049 <tontonkirikiri@gmail.com>
2026-09-19 15:04:23 +0900

    test(sandbox): add Vinyl Noise single section sound check target

--- build.zig
+++ build.zig
@@ -141,6 +141,7 @@ fn build_sandbox(
         .{ "sandbox/guitar/morning-brew.zig", "morning-brew.wav" },
         .{ "sandbox/rhodes/mono-440.0.zig", "rhodes-mono-440.0.wav" },
         .{ "sandbox/wood_bass/mono-bass-110.0.zig", "wood-bass-mono-110.0.wav" },
+        .{ "sandbox/vinyl_noise/crackle.zig", "vinyl-noise-crackle.wav" },
     };
 
     inline for (paths_names) |pn| {
