d21288aa92dbd1e4b9b195d29a9163d5885486ad
haruki7049 <tontonkirikiri@gmail.com>
2026-09-19 14:51:55 +0900

    feat(synthesizers): add continuous vinyl crackle noise generator

--- modules/synthesizers/root.zig
+++ modules/synthesizers/root.zig
@@ -5,6 +5,7 @@ pub const whitenoise = @import("./whitenoise/root.zig");
 pub const karplus_strong = @import("./karplus-strong/root.zig");
 pub const rhodes = @import("./rhodes/root.zig");
 pub const wood_bass = @import("./wood_bass/root.zig");
+pub const vinyl_noise = @import("./vinyl_noise/root.zig");
 
 test {
     std.testing.refAllDecls(@This());
