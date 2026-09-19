409ec7cab9ab4b34c45971506c5b008a71fb5f88
haruki7049 <tontonkirikiri@gmail.com>
2026-09-19 17:29:39 +0900

    feat(sandbox): add sandbox audio target for phrase 0006

--- build.zig
+++ build.zig
@@ -148,6 +148,7 @@ fn build_sandbox(
         .{ "sandbox/phrases/0003.zig", "phrase-0003.wav" },
         .{ "sandbox/phrases/0004.zig", "phrase-0004.wav" },
         .{ "sandbox/phrases/0005.zig", "phrase-0005.wav" },
+        .{ "sandbox/phrases/0006.zig", "phrase-0006.wav" },
     };
 
     inline for (paths_names) |pn| {
